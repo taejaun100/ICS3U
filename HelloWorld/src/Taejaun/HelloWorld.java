56ef0d01520890c0e0ff48803c0cd447a88bdc85
Taejaun Ayton <taejaun.ayton@student.tdsb.on.ca>
2016-09-12 13:09:33 -0400

    hi

---
+++ HelloWorld/src/Taejaun/HelloWorld.java
@@ -0,0 +1,6 @@
+package Taejaun;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
---
+++ HelloWorld/src/Taejaun/Hellol.java
@@ -0,0 +1,12 @@
+package Taejaun;
+/**
+* Displays a "Hello World!" message on the screen
+*/
+public class Hellol {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
