10eba0ee9d4f4d66c3a29ae571fdd143695395a5
Taejaun Ayton <taejaun.ayton@student.tdsb.on.ca>
2016-09-14 12:50:39 -0400

    hi

---
+++ AboutMe/src/ayton/AboutMe.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package ayton;
+
+/**
+ * @author 324117274
+ *
+ */
+public class AboutMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Full Name:Taejaun Ayton");
+		System.out.println("Ms.Kemp");
+		System.out.println("Runnymede C.I");
+		System.out.println("\"Go Ravens Go\"");
+	}
+
+}
