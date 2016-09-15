6e8a78663b1e3b3fdce99eb806c9755d8dafa5aa
Taejaun Ayton <taejaun.ayton@student.tdsb.on.ca>
2016-09-15 12:32:42 -0400

    hello

--- AboutMe/src/ayton/AboutMe.java
+++ AboutMe/src/ayton/AboutMe.java
@@ -18,6 +18,15 @@ public class AboutMe {
 		System.out.println("Ms.Kemp");
 		System.out.println("Runnymede C.I");
 		System.out.println("\"Go Ravens Go\"");
+		
+		System.out.format("%-10s %8s %8s","Period","Class\t"," Time\n\n");
+		System.out.format("%-10s %8s %8s","Period 1","BAF3M/BAT4M1-01"," 08:45AM-10:02AM\n");
+		System.out.format("%-10s %8s %8s","period 2","SCH3C/3U1-01\t","   10:07AM-11:24AM\n");
+		System.out.format("%-10s %8s %8s","Lunch"," \t "," \n");
+		System.out.format("%-10s %8s %8s","period 3","ICS3C/3U1-01\t","   12:26PM-01:35PM\n");
+		System.out.format("%-10s %8s %8s","period 4","PPL30m-01\t","   01:48PM-03:05PM\n");
+		
 	}
 
 }
+                                                                                                                                                                             
