cc88ce9089f7361ff1def823f629eb791126554c
Taejaun Ayton <taejaun.ayton@student.tdsb.on.ca>
2016-09-15 13:42:17 -0400

    hi

---
+++ AboutMe/src/ayton/Bingo.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package ayton;
+
+/**
+ * @author 324117274
+ *Taejaun Ayton 09/15/16 Bingo
+ */
+public class Bingo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n");
+		System.out.format("\t%4s %5s %5s %5s %5s","B","I","N","G","O\n");
+		System.out.format("\t%4s %5s %5s %5s %5s","2","20","42","60","64\n");
+		System.out.format("\t%4s %5s %5s %5s %5s","14","25","32","55","70\n");
+		System.out.format("\t%4s %5s %5s %5s %5s","5","18","FREE","53","67\n");
+		System.out.format("\t%4s %5s %5s %5s %5s","12","16","31","46","75\n");
+		System.out.format("\t%4s %5s %5s %5s %5s","10","22","39","59","71\n");
+	}
+
+}
---
+++ AboutMe/src/ayton/Bingo2.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package ayton;
+
+/**
+ * @author 324117274
+ *Taejaun Ayton 09/15/16 Bingo
+ */
+public class Bingo2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.format("1. The caller randomly pulls numbered bingo ball.\n");
+		System.out.format("\n");
+		System.out.format("2. The number is placed on the bingo board and called out.\n");
+		System.out.format("\n");
+		System.out.format("3. Players look for the called number on thier bingo card.\n");
+		System.out.format("\n");
+		System.out.format("4. If the number is located, it is marked off.\n");
+		System.out.format("\n");
+		System.out.format("5. Steps 1 to 4 are repeated untill a player matches the BINGO pattern.\n");
+		System.out.format("\n");
+		System.out.format("6. The winnging player yells BINGO.");
+	}
+
+}
